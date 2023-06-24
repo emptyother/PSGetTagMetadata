cc14907218ef475784504b5f5f2e21515872535f
Tom A. Vibeto <tom@onlyhuman.dk>
2023-06-24 18:01:37 +0200

    Tags are being returned from supported files.

--- PSGetTagMetadata/src/GetTagMetadata.cs
+++ PSGetTagMetadata/src/GetTagMetadata.cs
@@ -18,13 +18,13 @@ namespace OnlyHuman.PSGetTagMetadata
 		private string[] _paths;
 
 		private bool _shouldExpandWildcards;
+
 		[Parameter(
 			Mandatory = true,
 			ValueFromPipeline = false,
 			ValueFromPipelineByPropertyName = true,
 			ParameterSetName = ParamSetLiteral)
 		]
-
 		[Alias("PSPath")]
 		[ValidateNotNullOrEmpty]
 		public string[] LiteralPath
@@ -51,6 +51,8 @@ namespace OnlyHuman.PSGetTagMetadata
 			}
 		}
 
+		private string[] supportedFormats = new string[] { "bmp", "gif", "jpeg", "pbm", "pgm", "ppm", "pnm", "pcx", "png", "tiff", "dng", "svg", "jpg" };
+
 		protected override void ProcessRecord()
 		{
 			foreach (string path in _paths)
@@ -62,7 +64,7 @@ namespace OnlyHuman.PSGetTagMetadata
 				PSDriveInfo drive;
 				// this contains the paths to process for this iteration of the
 				// loop to resolve and optionally expand wildcards.
-				List<string> filePaths = new List<string>();
+				var filePaths = new List<string>();
 				if (_shouldExpandWildcards)
 				{
 					// Turn *.txt into foo.txt,foo2.txt etc.
@@ -85,23 +87,9 @@ namespace OnlyHuman.PSGetTagMetadata
 				// at this point, we have a list of paths on the filesystem.
 				foreach (string filePath in filePaths)
 				{
-					PSObject custom;
-					// If -whatif was supplied, do not perform the actions
-					// inside this "if" statement; only show the message.
-					//
-					// This block also supports the -confirm switch, where
-					// you will be asked if you want to perform the action
-					// "get metadata" on target: foo.txt
-					if (ShouldProcess(filePath, "Get Metadata"))
+					var custom = GetFileCustomObject(new FileInfo(filePath));
+					if (custom != null)
 					{
-						if (System.IO.Directory.Exists(filePath))
-						{
-							custom = GetDirectoryCustomObject(new DirectoryInfo(filePath));
-						}
-						else
-						{
-							custom = GetFileCustomObject(new FileInfo(filePath));
-						}
 						WriteObject(custom);
 					}
 				}
@@ -110,42 +98,47 @@ namespace OnlyHuman.PSGetTagMetadata
 
 		private PSObject GetFileCustomObject(FileInfo file)
 		{
-			// this message will be shown if the -verbose switch is given
-			WriteVerbose("GetFileCustomObject " + file);
-			// create a custom object with a few properties
-			PSObject custom = new PSObject();
-			custom.Properties.Add(new PSNoteProperty("Size", file.Length));
-			custom.Properties.Add(new PSNoteProperty("Name", file.Name));
-			custom.Properties.Add(new PSNoteProperty("Extension", file.Extension));
-			return custom;
-		}
+			WriteVerbose($"Processing {file.FullName}");
+			WriteVerbose($"Extension: {file.Extension.ToLower()}");
+			WriteVerbose($"Index: {Array.IndexOf(supportedFormats, file.Extension.Substring(1).ToLower())}");
+			if (file.Extension == null || Array.IndexOf(supportedFormats, file.Extension.Substring(1).ToLower()) == -1)
+			{
+				WriteVerbose($"File format is not supported");
+				WriteVerbose($"Supported formats: {string.Join(", ", supportedFormats)}");
+				return null;
+			}
+			WriteVerbose($"File format is supported");
+			try
+			{
+				var tfile = TagLib.File.Create(file.FullName);
+				var tag = tfile.Tag as TagLib.Image.CombinedImageTag;
+				var keywords = tag.Keywords;
+				WriteVerbose($"Keywords: {keywords}");
 
-		private PSObject GetDirectoryCustomObject(DirectoryInfo dir)
-		{
-			// this message will be shown if the -verbose switch is given
-			WriteVerbose("GetDirectoryCustomObject " + dir);
-			// create a custom object with a few properties
-			PSObject custom = new PSObject();
-			int files = dir.GetFiles().Length;
-			int subdirs = dir.GetDirectories().Length;
-			custom.Properties.Add(new PSNoteProperty("Files", files));
-			custom.Properties.Add(new PSNoteProperty("Subdirectories", subdirs));
-			custom.Properties.Add(new PSNoteProperty("Name", dir.Name));
-			return custom;
+				// New custom PSObject
+				var custom = new PSObject();
+				custom.Properties.Add(new PSNoteProperty("File", file));
+				custom.Properties.Add(new PSNoteProperty("Keywords", keywords));
+				WriteVerbose($"Custom object: {custom}");
+				return custom;
+			}
+			catch (Exception ex)
+			{
+				WriteError(new ErrorRecord(ex, "Error", ErrorCategory.InvalidOperation, file));
+				return null;
+			}
 		}
 
 		private bool IsFileSystemPath(ProviderInfo provider, string path)
 		{
-			bool isFileSystem = true;
+			var isFileSystem = true;
 			// check that this provider is the filesystem
 			if (provider.ImplementingType != typeof(FileSystemProvider))
 			{
 				// create a .NET exception wrapping our error text
-				ArgumentException ex = new ArgumentException(path +
-					" does not resolve to a path on the FileSystem provider.");
+				ArgumentException ex = new ArgumentException($"{path} does not resolve to a path on the FileSystem provider.");
 				// wrap this in a powershell errorrecord
-				ErrorRecord error = new ErrorRecord(ex, "InvalidProvider",
-					ErrorCategory.InvalidArgument, path);
+				ErrorRecord error = new ErrorRecord(ex, "InvalidProvider", ErrorCategory.InvalidArgument, path);
 				// write a non-terminating error to pipeline
 				this.WriteError(error);
 				// tell our caller that the item was not on the filesystem
